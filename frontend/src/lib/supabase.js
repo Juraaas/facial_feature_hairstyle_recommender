a3dc9483723f51cce296bed13c8b4b4a0f662fbd
Juraaas <jurewiczjuras@gmail.com>
2026-09-03 22:11:57 +0200

    add redirect after register

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -4,3 +4,14 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
+
+async function signUpWithEmail(email, password) {
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: 'https://stylizzer.vercel.app/analyse',
+    }
+  })
+  if (error) throw error
+}
